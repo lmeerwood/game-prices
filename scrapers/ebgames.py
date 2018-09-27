7421a52cf5e9a3f475e3a3813b2423787c1b0e77
Lenny Meerwood <Lenny.Meerwood@calvary.qld.edu.au>
2018-09-27 13:47:14 +1000

    Added more consoles to whitelist. Sorts results now.

--- scrapers/ebgames.py
+++ scrapers/ebgames.py
@@ -1,4 +1,5 @@
 from . import utils
+from decimal import Decimal
 import requests, bs4
 
 __URL = ("https://ebgames.com.au/any/any?q={}", "%20")
@@ -8,11 +9,28 @@ __ALLOWED_BRANDS = [
     'nintendo 3ds',
     'nintendo ds',
     'nintendo switch',
+    'playstation 4',
+    'playstation 3',
+    'playstation vita',
+    'xbox one',
+    'xbox 360',
 ]
 
 __DISALLOWED_CATEGORIES = [
     'accessories'
 ]
+
+__SYSTEM_CODES = {
+    'nintendo wii u': 'wiiu',
+    'nintendo 3ds': '3ds',
+    'nintendo ds': 'nds',
+    'nintendo switch': 'switch',
+    'playstation 4': 'ps4',
+    'playstation 3': 'ps3',
+    'playstation vita': 'vita',
+    'xbox one': 'xbone',
+    'xbox 360': 'x360',
+}
 
 def query(game, platform='any'):
     """
@@ -33,11 +51,17 @@ def query(game, platform='any'):
     for item in items:
         link = item.select('a')[0]
         name = link.attrs['data-name']
+        brand = link.attrs['data-brand'].lower()
+        price = Decimal(utils.priceClean(link.attrs['data-price']))
+
         correctGame = game.strip(' ').lower() in name.strip(' ').lower()
-        correctBrand = link.attrs['data-brand'].lower() in __ALLOWED_BRANDS
+        correctBrand =  brand in __ALLOWED_BRANDS
         correctCategory = not link.attrs['data-category'].lower() in __DISALLOWED_CATEGORIES
+
         if correctGame and correctCategory and correctBrand:
-            price = utils.priceClean(link.attrs['data-price'])
-            results.append((name, price))
+            results.append((name, __SYSTEM_CODES[brand], price))
     
-    return results
+    return sorted(results, key=getKeyPrice, reverse=True)
+
+def getKeyPrice(item):
+    return item[2], item[0]
